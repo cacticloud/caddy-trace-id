939bbff765e748ff9dc69e33d3646db5b67219e8
tony.z <tony.z@logtec.com>
2024-10-28 02:44:24 +0800

    feat: add basic

--- main.go
+++ main.go
@@ -1,44 +1,19 @@
 package caddytraceid
 
-import (
-	"fmt"
-	"log"
-	"math/rand"
-	"net/http"
-	"time"
-
-	"github.com/caddyserver/caddy/v2"
-	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
-)
+import "github.com/caddyserver/caddy/v2"
 
 func init() {
-	rand.Seed(time.Now().UnixNano()) // 初始化随机种子
-	caddy.RegisterModule(TraceID{})
+	caddy.RegisterModule(Gizmo{})
 }
 
-type TraceID struct{}
+// Gizmo只是一个例子；可以是你自己的类型
+type Gizmo struct {
+}
 
-// CaddyModule 返回 Caddy 模块的信息。
-func (TraceID) CaddyModule() caddy.ModuleInfo {
+// 通过CaddyModule方法返回Caddy模块的信息
+func (Gizmo) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
-		ID:  "http.handlers.trace_id",
-		New: func() caddy.Module { return new(TraceID) },
+		ID:  "foo.gizmo",
+		New: func() caddy.Module { return new(Gizmo) },
 	}
 }
-
-func (trace TraceID) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
-	log.Println("RequestID middleware is called.")
-	uniqueID := generateUniqueID()       // 生成唯一 ID
-	r.Header.Set("X-Trace-ID", uniqueID) // 设置请求头
-	return next.ServeHTTP(w, r)          // 继续执行下一个处理器
-}
-
-// generateUniqueID 使用当前时间和一个随机数生成一个唯一的 ID。
-func generateUniqueID() string {
-	now := time.Now()
-	return fmt.Sprintf("%d%06d", now.UnixNano(), rand.Intn(999999))
-}
-
-var (
-	_ caddyhttp.MiddlewareHandler = (*TraceID)(nil)
-)
