c4851fb0b76df30da34f078aa66eb1a82b24995d
tony.z <tony.z@logtec.com>
2024-10-28 03:39:32 +0800

    feat: add basic

--- main.go
+++ main.go
@@ -1,59 +1,50 @@
-package caddytraceid
+package caddy_extra
 
 import (
-	"context"
-	"net"
+	"net/http"
 
 	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
 )
 
-type Hello struct {
-	Address string `json:"address"`
-
-	listener net.Listener
-}
-
-func (Hello) CaddyModule() caddy.ModuleInfo {
-	return caddy.ModuleInfo{
-		ID:  "caddy_hello",
-		New: func() caddy.Module { return new(Hello) },
-	}
-}
-
 func init() {
-	caddy.RegisterModule(Hello{})
+	caddy.RegisterModule(ExtraInfo{})
+	httpcaddyfile.RegisterHandlerDirective("extra_info", parseCaddyfile)
 }
 
-func (h *Hello) Start() error {
-	network, err := caddy.ParseNetworkAddress(h.Address)
-	if err != nil {
-		return err
-	}
-
-	listener, err := network.Listen(context.Background(), 0, net.ListenConfig{})
-	if err != nil {
-		return err
-	}
-	h.listener = listener.(net.Listener)
-	go h.loop()
-	return nil
+// ExtraInfo 是一个简单的 HTTP 中间件，用于在响应中添加额外信息
+type ExtraInfo struct {
+	Message string `json:"message,omitempty"`
 }
 
-func (h *Hello) Stop() error {
-	return h.listener.Close()
-}
-
-func (h *Hello) loop() {
-	for {
-		c, err := h.listener.Accept()
-		if err != nil {
-			break
-		}
-		go h.handleConn(c)
+// CaddyModule 返回一个关于插件的信息
+func (ExtraInfo) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.handlers.extra_info",
+		New: func() caddy.Module { return new(ExtraInfo) },
 	}
 }
 
-func (h *Hello) handleConn(c net.Conn) {
-	_, _ = c.Write([]byte("Hello world"))
-	_ = c.Close()
+// ServeHTTP 实现 caddyhttp.MiddlewareHandler 接口
+func (e ExtraInfo) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	// 在响应头中设置额外信息
+	w.Header().Add("X-Extra-Info", e.Message)
+	return next.ServeHTTP(w, r) // 继续执行下一个处理器
+}
+
+var (
+	_ caddyhttp.MiddlewareHandler = (*ExtraInfo)(nil)
+)
+
+// parseCaddyfile 用于解析 Caddyfile 并配置 ExtraInfo 实例
+func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
+	var e ExtraInfo
+	if !h.Next() {
+		return nil, h.ArgErr()
+	}
+	if !h.AllArgs(&e.Message) {
+		return nil, h.ArgErr()
+	}
+	return e, nil
 }
