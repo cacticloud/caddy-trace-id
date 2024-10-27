ab973395ccc1b5c750ce3d79804564e62b3e7b8b
tony.z <tony.z@logtec.com>
2024-10-28 03:05:52 +0800

    feat: add basic

--- main.go
+++ main.go
@@ -1,19 +1,32 @@
 package caddytraceid
 
-import "github.com/caddyserver/caddy/v2"
+import (
+	"net/http"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+)
 
 func init() {
 	caddy.RegisterModule(Gizmo{})
 }
 
-// Gizmo只是一个例子；可以是你自己的类型
-type Gizmo struct {
-}
+type Gizmo struct{}
 
-// 通过CaddyModule方法返回Caddy模块的信息
+// CaddyModule 返回 Caddy 模块的信息，现在使用 http.handlers 命名空间
 func (Gizmo) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
-		ID:  "foo.gizmo",
+		ID:  "http.handlers.gizmo", // 修改这里以匹配 http.handlers
 		New: func() caddy.Module { return new(Gizmo) },
 	}
 }
+
+// ServeHTTP 实现 caddyhttp.MiddlewareHandler 接口
+func (g *Gizmo) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	// 实现你的处理逻辑
+	return next.ServeHTTP(w, r)
+}
+
+var (
+	_ caddyhttp.MiddlewareHandler = (*Gizmo)(nil) // 确保 Gizmo 实现了 MiddlewareHandler 接口
+)
