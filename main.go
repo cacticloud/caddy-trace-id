5b658b125cf0aab9bb011fea723f912e0e6df77a
tony.z <tony.z@logtec.com>
2024-10-29 00:08:41 +0800

    feat: add basic

--- main.go
+++ main.go
@@ -2,72 +2,54 @@ package caddy_req_id
 
 import (
 	"context"
-	"crypto/sha256"
-	"encoding/hex"
-	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/google/uuid"
 	"go.uber.org/zap"
 )
 
 func init() {
-	caddy.RegisterModule(UniqueID{})
+	caddy.RegisterModule(ReqID{})
 	httpcaddyfile.RegisterHandlerDirective("req_id", parseCaddyfile)
 }
 
-type UniqueID struct {
-	UserIDHeader string `json:"user_id_header,omitempty"` // 用户ID从哪个HTTP头部字段读取
-	Logger       *zap.Logger
+type ReqID struct {
+	Logger *zap.Logger
 }
 
-func (UniqueID) CaddyModule() caddy.ModuleInfo {
+func (ReqID) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
 		ID:  "http.handlers.req_id",
-		New: func() caddy.Module { return new(UniqueID) },
+		New: func() caddy.Module { return new(ReqID) },
 	}
 }
 
-// Provision set up the module's configuration
-func (u *UniqueID) Provision(ctx caddy.Context) error {
-	u.Logger = ctx.Logger(u) // Get the logger from the context
+func (u *ReqID) Provision(ctx caddy.Context) error {
+	u.Logger = ctx.Logger(u)
 	return nil
 }
 
-func (u UniqueID) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
-	userID := r.Header.Get(u.UserIDHeader)
-	if userID == "" {
-		userID = "anonymous"
-	}
+func (u ReqID) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	reqID := uuid.NewString()
+	r.Header.Set("Req-ID", reqID)
+	w.Header().Set("Req-ID", reqID)
 
-	uniqueID := generateUniqueID(time.Now(), r.RemoteAddr, userID, r.Method, r.URL.String())
-	r.Header.Set("X-Unique-ID", uniqueID) // 设置请求头，传递到后端
-
-	newContext := context.WithValue(r.Context(), "X-Unique-ID", uniqueID)
+	newContext := context.WithValue(r.Context(), "Req-ID", reqID)
 	newRequest := r.WithContext(newContext)
 
 	return next.ServeHTTP(w, newRequest)
 }
 
-func generateUniqueID(t time.Time, ip, userID, method, url string) string {
-	data := fmt.Sprintf("%v-%v-%v-%v-%v", t.UnixNano(), ip, userID, method, url)
-	hash := sha256.Sum256([]byte(data))
-	return hex.EncodeToString(hash[:])
-}
-
 var (
-	_ caddyhttp.MiddlewareHandler = (*UniqueID)(nil)
+	_ caddyhttp.MiddlewareHandler = (*ReqID)(nil)
 )
 
 func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
-	var u UniqueID
+	var u ReqID
 	if !h.Next() {
-		return nil, h.ArgErr()
-	}
-	if !h.AllArgs(&u.UserIDHeader) {
 		return nil, h.ArgErr()
 	}
 	return u, nil
